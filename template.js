72edc004e2f581e2c8603fad017acc2fb965189a
hdmariani <hermano.drummond@stape.io>
2025-11-26 08:05:06 -0300

    Persons API version bump. Added .gitignore and intellisense for SGTM. Removed unused code.

--- template.js
+++ template.js
@@ -4,14 +4,17 @@ const getRequestHeader = require('getRequestHeader');
 const encodeUriComponent = require('encodeUriComponent');
 const logToConsole = require('logToConsole');
 const getContainerVersion = require('getContainerVersion');
-const makeString = require('makeString');
 const makeTableMap = require('makeTableMap');
+const getTimestampMillis = require('getTimestampMillis');
+const BigQuery = require('BigQuery');
+const getAllEventData = require('getAllEventData');
 
-const containerVersion = getContainerVersion();
-const isDebug = containerVersion.debugMode;
-const isLoggingEnabled = determinateIsLoggingEnabled();
-const traceId = getRequestHeader('trace-id');
+/*==============================================================================
+==============================================================================*/
 
+const eventData = getAllEventData();
+
+checkGuardClauses();
 
 if (data.type === 'lead') {
   createLead(createPerson());
@@ -19,44 +22,42 @@ if (data.type === 'lead') {
   createPerson();
 }
 
+if (data.useOptimisticScenario) {
+  data.gtmOnSuccess();
+}
+
+/*==============================================================================
+VENDOR RELATED FUNCTIONS
+==============================================================================*/
+
 function createPerson() {
-  const requestUrl = 'https://api.pipedrive.com/v1/persons?api_token=' + enc(data.apiToken);
+  const requestUrl = 'https://api.pipedrive.com/v2/persons?api_token=' + enc(data.apiToken);
   const postBody = makeTableMap(data.person || [], 'field', 'value') || {};
 
   if (data.name) postBody.name = data.name;
-  if (data.email) postBody.email = [data.email];
-  if (data.phone) postBody.phone = [data.phone];
+  if (data.email) postBody.emails = [data.email];
+  if (data.phone) postBody.phones = [data.phone];
 
-  if (isLoggingEnabled) {
-    logToConsole(
-      JSON.stringify({
-        Name: 'PipeDrive',
-        Type: 'Request',
-        TraceId: traceId,
-        EventName: 'Person',
-        RequestMethod: 'POST',
-        RequestUrl: requestUrl,
-        RequestBody: postBody
-      })
-    );
-  }
+  log({
+    Name: 'PipeDrive',
+    Type: 'Request',
+    EventName: 'Person',
+    RequestMethod: 'POST',
+    RequestUrl: requestUrl,
+    RequestBody: postBody
+  });
 
   return sendHttpRequest(
     requestUrl,
     (statusCode, headers, body) => {
-      if (isLoggingEnabled) {
-        logToConsole(
-          JSON.stringify({
-            Name: 'PipeDrive',
-            Type: 'Response',
-            TraceId: traceId,
-            EventName: 'Person',
-            ResponseStatusCode: statusCode,
-            ResponseHeaders: headers,
-            ResponseBody: body
-          })
-        );
-      }
+      log({
+        Name: 'PipeDrive',
+        Type: 'Response',
+        EventName: 'Person',
+        ResponseStatusCode: statusCode,
+        ResponseHeaders: headers,
+        ResponseBody: body
+      });
 
       if (statusCode >= 200 && statusCode < 303) {
         if (data.type === 'lead') {
@@ -70,7 +71,7 @@ function createPerson() {
     },
     {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'POST'
@@ -85,36 +86,26 @@ function createLead() {
 
   if (data.leadName) postBody.name = data.leadName;
 
-  if (isLoggingEnabled) {
-    logToConsole(
-      JSON.stringify({
-        Name: 'PipeDrive',
-        Type: 'Request',
-        TraceId: traceId,
-        EventName: 'Lead',
-        RequestMethod: 'POST',
-        RequestUrl: requestUrl,
-        RequestBody: postBody
-      })
-    );
-  }
+  log({
+    Name: 'PipeDrive',
+    Type: 'Request',
+    EventName: 'Lead',
+    RequestMethod: 'POST',
+    RequestUrl: requestUrl,
+    RequestBody: postBody
+  });
 
   return sendHttpRequest(
     requestUrl,
     (statusCode, headers, body) => {
-      if (isLoggingEnabled) {
-        logToConsole(
-          JSON.stringify({
-            Name: 'PipeDrive',
-            Type: 'Response',
-            TraceId: traceId,
-            EventName: 'Lead',
-            ResponseStatusCode: statusCode,
-            ResponseHeaders: headers,
-            ResponseBody: body
-          })
-        );
-      }
+      log({
+        Name: 'PipeDrive',
+        Type: 'Response',
+        EventName: 'Lead',
+        ResponseStatusCode: statusCode,
+        ResponseHeaders: headers,
+        ResponseBody: body
+      });
 
       if (statusCode >= 200 && statusCode < 303) {
         data.gtmOnSuccess();
@@ -124,7 +115,7 @@ function createLead() {
     },
     {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'POST'
@@ -133,12 +124,99 @@ function createLead() {
   );
 }
 
+/*==============================================================================
+HELPERS
+==============================================================================*/
+
+function checkGuardClauses() {
+  const url = eventData.page_location || getRequestHeader('referer');
+
+  if (!isConsentGivenOrNotRequired(data, eventData)) {
+    return data.gtmOnSuccess();
+  }
+
+  if (url && url.lastIndexOf('https://gtm-msr.appspot.com/', 0) === 0) {
+    return data.gtmOnSuccess();
+  }
+}
+
 function enc(data) {
   data = data || '';
-  return encodeUriComponent(makeString(data));
+  return encodeUriComponent(data);
+}
+
+function isConsentGivenOrNotRequired(data, eventData) {
+  if (data.adStorageConsent !== 'required') return true;
+  if (eventData.consent_state) return !!eventData.consent_state.ad_storage;
+  const xGaGcs = eventData['x-ga-gcs'] || ''; // x-ga-gcs is a string like "G110"
+  return xGaGcs[2] === '1';
+}
+
+function log(rawDataToLog) {
+  const logDestinationsHandlers = {};
+  if (determinateIsLoggingEnabled()) logDestinationsHandlers.console = logConsole;
+  if (determinateIsLoggingEnabledForBigQuery()) logDestinationsHandlers.bigQuery = logToBigQuery;
+
+  rawDataToLog.TraceId = getRequestHeader('trace-id');
+
+  const keyMappings = {
+    // No transformation for Console is needed.
+    bigQuery: {
+      Name: 'tag_name',
+      Type: 'type',
+      TraceId: 'trace_id',
+      EventName: 'event_name',
+      RequestMethod: 'request_method',
+      RequestUrl: 'request_url',
+      RequestBody: 'request_body',
+      ResponseStatusCode: 'response_status_code',
+      ResponseHeaders: 'response_headers',
+      ResponseBody: 'response_body'
+    }
+  };
+
+  for (const logDestination in logDestinationsHandlers) {
+    const handler = logDestinationsHandlers[logDestination];
+    if (!handler) continue;
+
+    const mapping = keyMappings[logDestination];
+    const dataToLog = mapping ? {} : rawDataToLog;
+
+    if (mapping) {
+      for (const key in rawDataToLog) {
+        const mappedKey = mapping[key] || key;
+        dataToLog[mappedKey] = rawDataToLog[key];
+      }
+    }
+
+    handler(dataToLog);
+  }
+}
+
+function logConsole(dataToLog) {
+  logToConsole(JSON.stringify(dataToLog));
+}
+
+function logToBigQuery(dataToLog) {
+  const connectionInfo = {
+    projectId: data.logBigQueryProjectId,
+    datasetId: data.logBigQueryDatasetId,
+    tableId: data.logBigQueryTableId
+  };
+
+  dataToLog.timestamp = getTimestampMillis();
+
+  ['request_body', 'response_headers', 'response_body'].forEach((p) => {
+    dataToLog[p] = JSON.stringify(dataToLog[p]);
+  });
+
+  BigQuery.insert(connectionInfo, [dataToLog], { ignoreUnknownValues: true });
 }
 
 function determinateIsLoggingEnabled() {
+  const containerVersion = getContainerVersion();
+  const isDebug = !!(containerVersion && (containerVersion.debugMode || containerVersion.previewMode));
+
   if (!data.logType) {
     return isDebug;
   }
@@ -152,4 +230,9 @@ function determinateIsLoggingEnabled() {
   }
 
   return data.logType === 'always';
+}
+
+function determinateIsLoggingEnabledForBigQuery() {
+  if (data.bigQueryLogType === 'no') return false;
+  return data.bigQueryLogType === 'always';
 }
